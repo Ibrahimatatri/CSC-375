e93a860abbe3bfdaa622ed606753be206b35a4ef
Ibrahimatatri <ibrahimatatri@gmail.com>
2026-02-15 11:14:37 -0500

    Implement WordList class with dynamic array

--- src/WordList.cpp
+++ src/WordList.cpp
@@ -1,1 +1,39 @@
+#include "WordList.h"
+#include <fstream>
+
+WordList::WordList() {
+    capacity = 1024;
+    count = 0;
+    words = new string[capacity];
+}
+
+WordList::~WordList() {
+    delete[] words;
+    words = nullptr;
+    count = 0;
+    capacity = 0;
+}
+
+void WordList::add(const string& w) {
+    if (count >= capacity) {
+        int newCap = capacity * 2;
+        string* newArr = new string[newCap];
+        for (int i = 0; i < count; i++) newArr[i] = words[i];
+        delete[] words;
+        words = newArr;
+        capacity = newCap;
+    }
+    words[count++] = w;
+}
+
+WordList loadWordList(const string& path) {
+    WordList wl;
+    ifstream in(path);
+    string w;
+
+    while (in >> w) {
+        if ((int)w.size() == 5) wl.add(w);
+    }
+    return wl;
+}
 
