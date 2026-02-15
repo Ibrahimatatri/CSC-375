e9944fba857a300b8176a59fad939b5b3ddf16bf
Ibrahimatatri <ibrahimatatri@gmail.com>
2026-02-15 10:59:59 -0500

    Add Feedback namespace with grade and matches functions

--- src/Feedback.h
+++ src/Feedback.h
@@ -1,1 +1,10 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+namespace Feedback {
+    string grade(const string& guess, const string& secret);
+    bool matches(const string& guess, const string& feedback, const string& candidate);
+}
 
