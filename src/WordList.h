d9446e24bbf2191599afdfaaeead8494025ccb21
Ibrahimatatri <ibrahimatatri@gmail.com>
2026-02-15 11:14:17 -0500

    Add WordList structure and related functions
    Define WordList structure with methods for managing a list of words.

--- src/WordList.h
+++ src/WordList.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+struct WordList {
+    string* words;
+    int count;
+    int capacity;
+
+    WordList();
+    ~WordList();
+
+    void add(const string& w);
+};
+
+WordList loadWordList(const string& path);
 
