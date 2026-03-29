ab5fc2b9b912e0d15c094109475332235b7c9bd2
BigScosh <elwilkin3@gmail.com>
2026-03-28 20:02:10 -0400

    Update Solver.h

--- src/Solver.h
+++ src/Solver.h
@@ -1,1 +1,10 @@
+#pragma once
+#include <string>
 
+using namespace std;
+
+namespace Solver {
+    // Valid words are candidates[0..n-1]. Next guess must be one of them.
+    // Chooses the guess that minimizes expected survivors (uniform secret): min sum of n_f^2 over feedback buckets f.
+    string bestGreedyGuess(const string* candidates, int n);
+}
