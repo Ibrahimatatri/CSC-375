84b14a2e2f587a40bc3807b66cb98206532688fe
Ibrahimatatri <ibrahimatatri@gmail.com>
2026-03-28 20:47:35 -0400

    Refactor Solver.h to avoid using namespace std
    Removed using namespace std; and updated bestGreedyGuess declaration.

--- src/Solver.h
+++ src/Solver.h
@@ -1,10 +1,9 @@
 #pragma once
 #include <string>
 
-using namespace std;
-
 namespace Solver {
+    std::string bestGreedyGuess(const std::string* candidates, int n);
+}
     // Valid words are candidates[0..n-1]. Next guess must be one of them.
     // Chooses the guess that minimizes expected survivors (uniform secret): min sum of n_f^2 over feedback buckets f.
-    string bestGreedyGuess(const string* candidates, int n);
-}
+   
